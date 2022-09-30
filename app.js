47612c07d420aab233d2fcc2466c111ee37ceb0a
이찬우 <scflcweric1201@gmail.com>
2022-09-30 14:50:37 +0900

    Assignment 2 - 게시물 등록(생성) 엔드포인트

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js - 회원가입 엔드포인트 구현
+// app.js - 회원가입 엔드포인트 구현 - 게시글 생성 엔드포인트 구현
 
 const users = [
      {
@@ -42,16 +42,30 @@ const hRL = function(request, response){
                request.on('data', (data) => {body += data;})
                
                request.on('end', () => {
-                    const user = JSON.parse(body);
+                    const content = JSON.parse(body);
 
-                    users.push({
-                         id: user.id,
-                         name: user.name,
-                         email: user.email,
-                         password: user.password
-                    })
-                    response.writeHead(200, {'Content-Type':"application/json"});
-                    response.end(JSON.stringify({ message: 'userCreated'}));
+                    if(url=='/users'){
+                         users.push({
+                              id: content.id,
+                              name: content.name,
+                              email: content.email,
+                              password: content.password
+                         })
+                         response.writeHead(200, {'Content-Type':"application/json"});
+                         response.end(JSON.stringify({ message: 'userCreated'}));
+                    }
+                    else if (url=='/posts'){
+                         posts.push({
+                              id: content.id,
+                              title: content.title,
+                              content: content.content,
+                              userId: content.userId
+                         })
+                         response.writeHead(200, {'Content-Type':"application/json"});
+                         response.end(JSON.stringify({ message: 'postCreated'}));
+                    }
+
+                    
                })
           }
 };
